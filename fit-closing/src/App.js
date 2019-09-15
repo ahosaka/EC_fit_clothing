7bac0e6ecb411ca27e043e80df6e4034856f473a
Atsushi Hosaka <ahosaka@indeed.com>
2019-09-15 17:52:46 -0500

    update the app with a shop page, collection preview component and collection item component

--- fit-closing/src/App.js
+++ fit-closing/src/App.js
@@ -3,12 +3,9 @@ import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
+
 
 function App() {
   return (
@@ -16,7 +13,7 @@ function App() {
       <Switch>
 
         <Route exact path = '/' component = {HomePage} />
-        <Route path = '/hats' component = {HatsPage} />
+        <Route path = '/shop' component = {ShopPage} />
 
       </Switch>
     </div>
